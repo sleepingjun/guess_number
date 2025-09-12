2a86cdb7e1e548364a8213789265b3059435157f
ani <best19980102@gmail.com>
2025-09-12 21:06:54 +0800

    mode 1 (easy) with fixed goal num.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//猜數字遊戲
+System.Console.WriteLine("Welcome~ Let's play a guess number game~");
+System.Console.Write("First of all, please enter your name: ");
+string name = System.Console.ReadLine();
+System.Console.Write($"Hello {name}~\nDo you dare to accept the challenge? (Y/N)");
+bool recall = System.Console.ReadLine().ToUpper() == "Y";
+int num_min = 1;
+int guess;
+
+if (recall==true)
+{
+    System.Console.WriteLine("WOW! What a brave man.\n There are different mode in our game, which do you want to challenge?");
+}
+else
+{
+    System.Console.WriteLine("Oh... Are you a coward? It's okay, I can understand:> \nByeeeeeee.");
+    return;
+}
+
+System.Console.WriteLine("1. Easy mode: Range(1~100) with 5 times to guess.\n2. Normal mode: Range(1~500) with 20 times to guess.\n3. Hard mode: Range(1~1000) with 25 times to guess.");
+System.Console.Write("Please enter the mode number (1, 2, or 3): ");
+int mode = System.Convert.ToInt32(System.Console.ReadLine());
+
+if (mode == 1)
+{
+    System.Console.WriteLine("You have chosen Easy mode. Have fun! :>");
+    int num_max = 100;
+    int goal = 36; //test, I'll change to be random number.
+    int guess_times = 0;
+    int guess_limit = 5;
+
+    do
+    {
+        System.Console.WriteLine($"This is your {guess_times + 1} guess. \nGuess a number between {num_min} ~ {num_max}: ");
+        guess = System.Convert.ToInt32(System.Console.ReadLine());
+        guess_times++;
+
+        if (guess < goal)
+        {
+            num_min = guess;
+            System.Console.WriteLine("Tt's too small!");
+        }
+        else if (guess > goal)
+        {
+            num_max = guess;
+            System.Console.WriteLine("Tt's too big!");
+        }
+        else
+        {
+            System.Console.WriteLine("You guessed it!!!");
+            break;
+        }
+    }
+    while(guess_times < guess_limit);
+    //while (guess_times==guess_limit)
+}
+else if (mode == 2)
+{
+    System.Console.WriteLine("You have chosen Normal mode. Good luck:>");
+    return;
+
+}
+else if (mode == 3)
+{
+    System.Console.WriteLine("You have chosen Hard mode. Good luck, you really need it:>");
+    return;
+
+}
+else
+{
+    System.Console.WriteLine("Invalid mode. Please restart the game and choose a valid mode. Or... Are you afraid?");
+}
+
