162f8c1d44f9c70ff549fe569054a9226b717617
ani <best19980102@gmail.com>
2025-09-27 17:44:55 +0800

    change target num to be random number. add win/lose kitty~

--- Program.cs
+++ Program.cs
@@ -1,139 +1,165 @@
 ﻿//猜數字遊戲
-System.Console.WriteLine("Welcome~ Let's play a guess number game~");
-System.Console.Write("First of all, please enter your name: ");
-string name = System.Console.ReadLine();
-System.Console.Write($"Hello {name}~\nDo you dare to accept the challenge? (Y/N)");
-bool recall = System.Console.ReadLine().ToUpper() == "Y";
+using System;
+Console.WriteLine("Welcome~ Let's play a guess number game~");
+Console.Write("First of all, please enter your name: ");
+string name = Console.ReadLine();
+Console.Write($"Hello {name}~\nDo you dare to accept the challenge? (Y/N)");
+bool recall = Console.ReadLine().ToUpper() == "Y";
 int num_min = 1;
 int guess;
-
+Random target = new Random();
 if (recall==true)
 {
-    System.Console.WriteLine("WOW! What a brave man.\n There are different mode in our game, which do you want to challenge?");
+    Console.WriteLine("WOW! What a brave man.\n There are different mode in our game, which do you want to challenge?");
 }
 else
 {
-    System.Console.WriteLine("Oh... Are you a coward? It's okay, I can understand:> \nByeeeeeee.");
+    Console.WriteLine("Oh... Are you a coward? It's okay, I can understand:> \nByeeeeeee.");
     return;
 }
 
-System.Console.WriteLine("1. Easy mode: Range(1~100) with 5 times to guess.\n2. Normal mode: Range(1~500) with 7 times to guess.\n3. Hard mode: Range(1~1000) with 10 times to guess.");
-System.Console.Write("Please enter the mode number (1, 2, or 3): ");
-int mode = System.Convert.ToInt32(System.Console.ReadLine());
+Console.WriteLine("1. Easy mode: Range(1~100) with 5 times to guess.\n2. Normal mode: Range(1~500) with 7 times to guess.\n3. Hard mode: Range(1~1000) with 10 times to guess.");
+Console.Write("Please enter the mode number (1, 2, or 3): ");
+int mode = Convert.ToInt32(Console.ReadLine());
 
 if (mode == 1)
 {
-    System.Console.WriteLine("You have chosen Easy mode. Have fun! :>");
+    Console.WriteLine("You have chosen Easy mode. Have fun! :>");
     int num_max = 100;
-    int goal = 36; //test, I'll change to be random number.
+    int goal = target.Next(0, 101);
     int guess_times = 0;
     int guess_limit = 5;
 
     do
     {
-        System.Console.WriteLine($"This is your {guess_times + 1} guess. \nGuess a number between {num_min} ~ {num_max}: ");
-        guess = System.Convert.ToInt32(System.Console.ReadLine());
+        Console.WriteLine($"This is your {guess_times + 1} guess. \nGuess a number between {num_min} ~ {num_max}: ");
+        guess = Convert.ToInt32(Console.ReadLine());
         guess_times++;
 
         if (guess < goal)
         {
             num_min = guess;
-            System.Console.WriteLine("It's too small!");
+            Console.WriteLine("It's too small!");
         }
         else if (guess > goal)
         {
             num_max = guess;
-            System.Console.WriteLine("It's too big!");
+            Console.WriteLine("It's too big!");
         }
         else
         {
-            System.Console.WriteLine("You guessed it!!!");
+            Console.WriteLine("You guessed it!!!");
+            Console.WriteLine(" /\\_______/\\");
+            Console.WriteLine("/   >   <   \\");
+            Console.WriteLine("\\_____V_____/");
             break;
         }
     }
     while(guess_times < guess_limit);
         if (guess_times == guess_limit)
         {
-            System.Console.WriteLine("You have used all your guesses. Game over!");
-        }
+            Console.WriteLine("You have used all your guesses. Game over!");
+            Console.WriteLine(" /\\_______/\\");
+            Console.WriteLine("/   Q   Q   \\");
+            Console.WriteLine("\\_____Δ____/");
+    }
 }
 
 else if (mode == 2)
 {
-    System.Console.WriteLine("You have chosen Normal mode. Good luck:>");
+    Console.WriteLine("You have chosen Normal mode. Good luck:>");
     int num_max = 500;
-    int goal = 376; //test, I'll change to be random number.
+    int goal = target.Next(0, 501);
     int guess_times = 0;
     int guess_limit = 7;
 
     do
     {
-        System.Console.WriteLine($"This is your {guess_times + 1} guess. \nGuess a number between {num_min} ~ {num_max}: ");
-        guess = System.Convert.ToInt32(System.Console.ReadLine());
+        Console.WriteLine($"This is your {guess_times + 1} guess. \nGuess a number between {num_min} ~ {num_max}: ");
+        guess = Convert.ToInt32(Console.ReadLine());
         guess_times++;
 
         if (guess < goal)
         {
             num_min = guess;
-            System.Console.WriteLine("It's too small!");
+            Console.WriteLine("It's too small!");
         }
         else if (guess > goal)
         {
             num_max = guess;
-            System.Console.WriteLine("It's too big!");
+            Console.WriteLine("It's too big!");
         }
         else
         {
-            System.Console.WriteLine("You guessed it!!!");
+            Console.WriteLine("You guessed it!!!");
+            Console.WriteLine(" /\\_______/\\");
+            Console.WriteLine("/   >   <   \\");
+            Console.WriteLine("\\_____V_____/");
             break;
         }
     }
     while (guess_times < guess_limit);
     if (guess_times == guess_limit)
     {
-        System.Console.WriteLine("You have used all your guesses. Game over!");
+        Console.WriteLine($"You have used all your guesses. The right number is {goal} ~ Game over!");
+        Console.WriteLine(" /\\_______/\\");
+        Console.WriteLine("/   Q   Q   \\");
+        Console.WriteLine("\\_____Δ____/");
     }
 
 }
 else if (mode == 3)
 {
-    System.Console.WriteLine("You have chosen Hard mode. Good luck, you really need it:>");
+    Console.WriteLine("You have chosen Hard mode. Good luck, you really need it:>");
     int num_max = 1000;
-    int goal = 762; //test, I'll change to be random number.
+    int goal = target.Next(0, 1001);
     int guess_times = 0;
     int guess_limit = 10;
 
     do
     {
-        System.Console.WriteLine($"This is your {guess_times + 1} guess. \nGuess a number between {num_min} ~ {num_max}: ");
-        guess = System.Convert.ToInt32(System.Console.ReadLine());
+        Console.WriteLine($"This is your {guess_times + 1} guess. \nGuess a number between {num_min} ~ {num_max}: ");
+        guess = Convert.ToInt32(Console.ReadLine());
         guess_times++;
 
         if (guess < goal)
         {
             num_min = guess;
-            System.Console.WriteLine("It's too small!");
+            Console.WriteLine("It's too small!");
         }
         else if (guess > goal)
         {
             num_max = guess;
-            System.Console.WriteLine("It's too big!");
+            Console.WriteLine("It's too big!");
         }
         else
         {
-            System.Console.WriteLine("You guessed it!!!");
+            Console.WriteLine("You guessed it!!!");
+            Console.WriteLine(" /\\_______/\\");
+            Console.WriteLine("/   >   <   \\");
+            Console.WriteLine("\\_____V_____/");
             break;
         }
     }
     while (guess_times < guess_limit);
     if (guess_times == guess_limit)
     {
-        System.Console.WriteLine("You have used all your guesses. Game over!");
+        Console.WriteLine($"You have used all your guesses. The right number is {goal} ~ Game over!");
+        Console.WriteLine(" /\\_______/\\");
+        Console.WriteLine("/   Q   Q   \\");
+        Console.WriteLine("\\_____Δ____/");
     }
 
 }
 else
 {
-    System.Console.WriteLine("Invalid mode. Please restart the game and choose a valid mode. Or... Are you afraid?");
+    Console.WriteLine("Invalid mode. Please restart the game and choose a valid mode. Or... Are you afraid?");
 }
 
+//Console.WriteLine(" /\\_______/\\");
+//Console.WriteLine("/   Q   Q   \\");
+//Console.WriteLine("\\_____Δ____/");
+
+//Console.WriteLine(" /\\_______/\\");
+//Console.WriteLine("/   >   <   \\");
+//Console.WriteLine("\\_____V_____/");
